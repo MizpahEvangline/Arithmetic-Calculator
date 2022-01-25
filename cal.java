a9b3cb8bf4a588680ddd8d94f8c105a71f989c12
MizpahEvangline <98093587+MizpahEvangline@users.noreply.github.com>
2022-01-25 12:46:25 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,46 @@
+package arithCal;
+
+import java.util.Scanner;
+
+public class cal
+{
+  public static void main(String[] args)
+  {
+	char operator;
+	double num1, num2, result;
+	
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("Choose an operator: +, -, *, or /");
+	operator=sc.next().charAt(0);
+	
+	System.out.println("Enter the first number");
+	num1=sc.nextDouble();
+	
+	System.out.println("Enter the second number");
+	num2=sc.nextDouble();
+	
+	switch(operator)
+	{
+	case '+':result=num1+num2;
+	System.out.println(num1 + "+" + num2 + "=" + result);
+	break;
+	
+	case '-':result=num1-num2;
+	System.out.println(num1 + "-" + num2 + "=" + result);
+	break;
+	
+	case '*':result=num1*num2;
+	System.out.println(num1 + "*" + num2 + "=" + result);
+	break;
+	
+	case '/':result=num1/num2;
+	System.out.println(num1 + "/" +num2 + "=" + result);
+	break;
+	
+	default:System.out.println("Invalid Operator");
+	break;
+	}
+	sc.close();
+  }
+}
